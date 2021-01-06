6411edc0589a9a4b61619db2d7151f7b6913b445
zo11o <372526745@qq.com>
2021-01-06 17:42:15 +0800

    first commit

---
+++ src/page/app/index.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/app.js
@@ -0,0 +1,12 @@
+// 解决在ios下title不更新的问题
+export function refreshTitle() {
+    if (navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)) {
+        const hackIframe = document.createElement('iframe')
+        hackIframe.style.display = 'none'
+        hackIframe.src = '/fixIosTitle.html?r=' + Math.random()
+        document.body.appendChild(hackIframe)
+        setTimeout(() => {
+            document.body.removeChild(hackIframe)
+        }, 300)
+    }
+}
