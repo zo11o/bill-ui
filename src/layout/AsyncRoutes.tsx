3f608acb072f8b5fd525503c9b28e4e7a6cb2dd3
zo11o <372526745@qq.com>
2021-01-13 23:35:35 +0800

    fix: 写法不一致

--- src/layout/AsyncRoutes.tsx
+++ src/layout/AsyncRoutes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TransitionMain from "components/TransitionMain";
-import { connet } from "react-redux";
+import { connect } from "react-redux";
 interface AsyncRoutesProps {
   children: React.ReactNode;
   // init: boolean;
